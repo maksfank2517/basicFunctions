c33656f302f24cd0be0280659b61e9f5470ace0c
MaksFank <mytestgit@admin.com>
2026-02-10 21:57:02 +0300

    Bug fixes

--- index.php
+++ index.php
@@ -6,12 +6,17 @@ $surname = trim(fgets(STDIN));
 echo 'Введите отчество: ';
 $patronymic = trim(fgets(STDIN));
 
-$fullname = mb_ucfirst($surname) . ' ' . mb_ucfirst($name) . ' ' . mb_ucfirst($patronymic);
-$fio = mb_strtoupper(mb_substr($surname, 0 , 1)) . mb_strtoupper(mb_substr($name, 0 , 1)) . mb_strtoupper(mb_substr($patronymic, 0 , 1));
-$surnameAndInitials = mb_ucfirst($surname) . ' ' . mb_strtoupper(mb_substr($name, 0 , 1)) . '.' . mb_strtoupper(mb_substr($patronymic, 0 , 1)) . '.';
+$fullname = mb_strtoupper(mb_substr($surname, 0 , 1, 'UTF-8'), 'UTF-8') . mb_strtolower(mb_substr($surname, 1 , null, 'UTF-8'), 'UTF-8') . ' ' .
+            mb_strtoupper(mb_substr($name, 0 , 1, 'UTF-8'), 'UTF-8') . mb_strtolower(mb_substr($name, 1 , null, 'UTF-8'), 'UTF-8') . ' ' .
+            mb_strtoupper(mb_substr($patronymic, 0 , 1, 'UTF-8'), 'UTF-8') . mb_strtolower(mb_substr($patronymic, 1 , null, 'UTF-8'), 'UTF-8');
+
+$fio = mb_strtoupper(mb_substr($surname, 0 , 1, 'UTF-8'), 'UTF-8') . mb_strtoupper(mb_substr($name, 0 , 1, 'UTF-8'), 'UTF-8') . 
+        mb_strtoupper(mb_substr($patronymic, 0 , 1,'UTF-8'), 'UTF-8');
+
+$surnameAndInitials = mb_strtoupper(mb_substr($surname, 0 , 1, 'UTF-8'), 'UTF-8') . mb_strtolower(mb_substr($surname, 1 , null, 'UTF-8'), 'UTF-8') . ' ' . 
+                        mb_strtoupper(mb_substr($name, 0 , 1, 'UTF-8'), 'UTF-8') . '.' . mb_strtoupper(mb_substr($patronymic, 0 , 1, 'UTF-8'), 'UTF-8') . '.';
 
 echo "Полное имя: '$fullname'" . PHP_EOL;
 echo "Фамилия и инициалы: '$surnameAndInitials'" . PHP_EOL;
-echo "Аббревиатура: '$fio' \\" . PHP_EOL;
-
+echo "Аббревиатура: '$fio'"  . PHP_EOL;
 ?>
